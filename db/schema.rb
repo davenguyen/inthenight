d27f4925ae786931a4248cb91ae4f58fc179bc9d
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-25 02:25:31 -0700

    Install lunar_shell

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160625092456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lunar_shell_users", force: :cascade do |t|
+    t.string   "username",        limit: 32,                 null: false
+    t.string   "password_digest",                            null: false
+    t.boolean  "admin",                      default: false, null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+  end
+
+  add_index "lunar_shell_users", ["username"], name: "index_lunar_shell_users_on_username", unique: true, using: :btree
 
 end
